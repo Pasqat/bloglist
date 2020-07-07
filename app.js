5108083f29e3440f0bac1d0a4d86b2e919e6e498
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2020-07-07 09:04:26 +0200

    blog controlles and models, refactored app.js

--- app.js
+++ app.js
@@ -1,48 +1,22 @@
 require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-console.log('connecting to', mongoUrl)
-
+logger.info('connecting to ', config.MONGODB_URI)
 mongoose
-  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('connected to MongoDB'))
   .catch(error => console.error('error connecting to MongoDB', error.message))
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
